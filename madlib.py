1e649ef2deacb2506fe482f7e3ba3d65a5c056f4
s203 <s203@AI241LAB0203M24.local>
2017-08-03 16:11:53 -0700

    updates

---
+++ madlib.py
@@ -0,0 +1,11 @@
+print ('Input Noun')
+noun = input()
+print ('Input Adjective ')
+adjective = input()
+print(' Input another Noun')
+another_noun = input()
+
+
+
+sentence = "The " + str(noun) + " jumped over the " + str(adjective) + " " + str(another_noun)
+print(sentence)
